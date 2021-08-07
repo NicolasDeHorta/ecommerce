7b4ff498828775b5d2a865a2c04ba2cc7b9a08db
NicolasDeHorta <dehorta.n@gmail.com>
2021-08-07 20:19:09 -0300

    agrego login system

--- js/login.js
+++ js/login.js
@@ -3,4 +3,36 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
 
+    var usersList = [
+    {
+        mail: "dehorta.n@gmail.com",
+        pass: "hola1234"
+    },
+    {
+        mail: "nicodh1103@gmail.com",
+        pass: "chau4321"
+    }
+]
+
+var getLoginInfo = () => {
+    let mail = $("[name='email-address']").val();
+    let pass = $("[name='password']").val();
+
+    return { 
+        mail: mail,
+        pass: pass
+    }
+}
+
+
+var login = () => {
+    let tryUser = getLoginInfo()
+    var registeredUser = usersList.filter(user => (user.mail === tryUser.mail && user.pass === tryUser.pass))   
+
+    registeredUser.length === 1 ? window.location.href = "./index.html" : alert("parece que no estas registrado o el usuario es Incorrecto")
+}
+
+
+$("#login-button").click(login)
+
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,51 @@
+var productsArray = [];
+
+function showProductsList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let product = array[i];
+        console.log(product)
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" alt="` + product.desc + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ product.name +`</h4>
+                        <small class="text-muted">` + product.soldCount + ` vendidos</small>
+                    </div>
+                    <br><p class="text-muted">` + product.description +` </p>
+                </div>
+                
+            </div>
+        </div>
+        `
+
+        document.getElementById("products-list").innerHTML = htmlContentToAppend;
+    }
+}
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    showSpinner()
+    
+    setTimeout(() =>getJSONData(PRODUCTS_URL)
+    .then(response => {
+        if (response.status === "ok")
+        {
+            productsArray = response.data;
 
+            showProductsList(productsArray);
+            console.log(productsArray)
+        }
+    })
+    ,500)
 });
