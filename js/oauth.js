b882b7baf6f5fa64c0940e360e83eb017ca75f05
NicolasDeHorta <dehorta.n@gmail.com>
2021-08-14 22:01:46 -0300

    trying to add oauth v12 add color to buttons and redirect

--- js/oauth.js
+++ js/oauth.js
@@ -64,12 +64,13 @@ var GoogleAuth;
         $('#auth-status').html('You are currently signed in and have granted ' +
             'access to this app.');
         $('#continue-access-button').css('display', 'inline-block')
-        $('#continue-access-button').click(() => window.location.href = "./home.html");
+        $('#continue-access-button').click(() => console.log("triggered"));
       } else {
         $('#sign-in-or-out-button').html('Sign In/Authorize');
         $('#revoke-access-button').css('display', 'none');
         $('#auth-status').html('You have not authorized this app or you are ' +
             'signed out.');
+        $('#continue-access-button').css('display', 'none')
       }
     }
   
