3b5bc0f2a4c08cd7dcdb53d08646c7b7561bd30f
NicolasDeHorta <dehorta.n@gmail.com>
2021-08-23 22:01:37 -0300

    Filtros agregados, todos los sort y de texto con keydown

--- js/products.js
+++ js/products.js
@@ -1,11 +1,26 @@
+let productsArray = []
+
+
+var getData = () => {
+    //funcion que recopila los datos del URL
+let arr = []
 fetch(PRODUCTS_URL)
 .then(res => res.json())
 .then(data => {
 
+    data.forEach((item) => arr.push(item))
+
+    productsArray = arr
+    showList(productsArray)
+})
+}
+
+
+var showList = list => {
+    // Funcion que muestre la lista actual del array de productos
     let htmlContentToAppend = "";
 
-    data.forEach(product => {
-
+    list.forEach(product => {
         htmlContentToAppend += `
         <div class="list-group-item list-group-item-action">
             <div class="row">
@@ -25,5 +40,66 @@ fetch(PRODUCTS_URL)
         </div>
         `
         })
+        
         $("#products-list").html(htmlContentToAppend) 
-})
+
+}
+
+
+var sortList = parameter => {
+    switch (parameter) {
+        case 'priceAsc':
+            productsArray.sort((a, b) => parseInt(a.cost) - parseInt(b.cost))
+            break;
+        case 'priceDesc':
+            productsArray.sort((a, b) => parseInt(b.cost) - parseInt(a.cost))
+            break;
+        case 'alphabeticalAsc':
+            productsArray.sort((a,b) => a.name > b.name ? 1 : -1)
+            break;
+        case 'alphabeticalDesc':
+            productsArray.sort((a,b) => a.name < b.name ? 1 : -1)
+            break;
+        case 'filterReset':
+            getData()
+            $("#filterMax").val(undefined)
+            $("#filterMin").val(undefined)
+            break;
+    }
+    showList(productsArray)
+}
+
+
+var filterList = () => {
+    
+    let min = $("#filterMin").val()
+    let max = $("#filterMax").val()
+    
+    if (min){
+        if (max){
+        showList(productsArray.filter(item => {return (parseInt(item.cost) < max && parseInt(item.cost) > min)} ))
+        } else {
+            showList(productsArray.filter(item => {return (parseInt(item.cost) > min)} ))
+        }
+    } else if (max) {
+        showList(productsArray.filter(item => {return (parseInt(item.cost) < max)} ))
+    }
+}
+
+var filterText = () => {
+    let text = $("#search-bar").val().toLowerCase()
+    text != undefined ? showList(productsArray.filter(item => {return ( item.name.toLowerCase().includes(text) || item.description.toLowerCase().includes(text))} )) : null
+}
+
+
+//Botones de filtros
+$("#filter-price-ascending").click(() => sortList('priceAsc')) 
+$("#filter-price-descending").click(() => sortList('priceDesc')) 
+$("#filter-alphabetical").click(() => sortList('alphabeticalAsc')) 
+$("#filter-anti-alphabetical").click(() => sortList('alphabeticalDesc')) 
+$("#filter-reset").click(() => sortList('filterReset'))
+$("#price-range").click(() => filterList())
+$("#search-bar").keydown(() => filterText())
+
+getData()
+
