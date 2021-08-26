fde29271476d741b806984b48a6396d7510cb919
NicolasDeHorta <dehorta.n@gmail.com>
2021-08-26 19:09:38 -0300

    agregado nombre de usuario con sessionStorage y con google auth geteando mail

--- js/oauth.js
+++ js/oauth.js
@@ -64,7 +64,7 @@ var GoogleAuth;
         $('#auth-status').html('Ya has dado permisos de acceso de Google a esta App');
         $('#continue-access-button').css('display', 'inline-block')
         $('#continue-access-button').click(() => {
-          sessionStorage.setItem('user', GoogleAuth.currentUser.get())
+          sessionStorage.setItem('user', GoogleAuth.currentUser.email)
           window.location.href = "./home.html"
       })
      } else {
