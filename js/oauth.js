5fe947fa50a1fdcf97e508bf6e42465a49f51cbf
NicolasDeHorta <dehorta.n@gmail.com>
2021-08-26 19:23:02 -0300

    intento de catch del mail de google auth

--- js/oauth.js
+++ js/oauth.js
@@ -64,7 +64,7 @@ var GoogleAuth;
         $('#auth-status').html('Ya has dado permisos de acceso de Google a esta App');
         $('#continue-access-button').css('display', 'inline-block')
         $('#continue-access-button').click(() => {
-          sessionStorage.setItem('user', user.getBasicProfile().getEmail())
+          sessionStorage.setItem('user', user.SCOPE.email)
           window.location.href = "./home.html"
       })
      } else {
