fd3ca5e9790b8df0d17371468ad7568db3c8b54c
NicolasDeHorta <dehorta.n@gmail.com>
2021-09-05 19:37:16 -0300

    sistema de comentarios y rating completo

--- js/product-info.js
+++ js/product-info.js
@@ -28,28 +28,37 @@ fetch(PRODUCT_INFO_URL)
     })
 })
 
-var addComment = (comment) => {
+var addComment = (name, score, comment, date) => {
 
-    let commentCard = `<div class="comment-card container"><h4>${comment.user} &nbsp; <span class="small">${comment.dateTime}</span></h4>`
+    let commentCard = `<div class="comment-card container"><h4>${name} &nbsp; <span class="small">${date}</span></h4>`
 
     for (let i=0; i<5; i++) {
-        if (i<comment.score) {
+        if (i<score) {
             commentCard += `<span class="fa fa-star checked "></span>`
         } else {
             commentCard += `<span class="fa fa-star "></span>`
         }
     }
 
-    commentCard += `<p>${comment.description}</p></div>`
+    commentCard += `<p>${comment}</p></div>`
 
 
     $(".comments-box").append(commentCard)
 }
+
+var publishComment = () => {
+ let score = $("input[name='rating']:checked").val()
+ let comment = $("textarea").val()
+ let date = new Date()
+    addComment(sessionStorage.getItem('user'), score, comment, date.toLocaleString())
+}
+
+$(".comment-card button").click(publishComment)
 
 
 fetch(PRODUCT_INFO_COMMENTS_URL)
 .then(res => res.json())
 .then(data => {
     console.log(data)
-    data.forEach(comment => {addComment(comment)})
+    data.forEach(comment => {addComment(comment.user, comment.score, comment.description, comment.dateTime)})
 })
